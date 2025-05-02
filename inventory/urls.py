e2aef01e9aaf42a6deaa6b82e80f37608f873b8a
JosephGabriels <josephgmugure@gmail.com>
2025-05-02 12:53:56 +0300

    adding api paths

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,9 +6,15 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import ReactAppView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('inventory_api.urls')),
 ]
 
